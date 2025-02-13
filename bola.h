82383ed2a0c44bd55e3d03bab65458324e42c530
Brokkensteel <goncalo_17@hotmail.com>
2025-02-13 19:55:42 +0000

    0.1

---
+++ bola.h
@@ -0,0 +1,19 @@
+#ifndef BOLA_H_INCLUDED
+#define BOLA_H_INCLUDED
+
+
+typedef struct{
+
+float posY;
+float posX;
+float _size;
+float speed;
+
+}Ball;
+
+
+
+
+
+
+#endif // BOLA_H_INCLUDED
--- main.c
+++ main.c
@@ -1,8 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "raylib.h"
+#include "bola.h"
 
-int main()
+
+
+
+
+
+
+int main(void)
 {
-    printf("Hello world!\n");
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1024;
+    const int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+
+    // TODO: Initialize all required variables and load all required data here!
+
+    int framesCounter = 0;          // Useful to count frames
+
+    SetTargetFPS(120);               // Set desired framerate (frames-per-second)
+    //--------------------------------------------------------------------------------------
+
+    Ball ball;
+    ball.posX = 512;
+    ball.posY = 360;
+    ball._size = 10;
+    ball.speed = 360;
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+
+        ball.posX += (ball.speed * (float)GetFrameTime());
+        if(ball.posX >= 1024 || ball.posX <= 0)
+        {
+            ball.speed *= -1;
+        }
+
+
+
+
+
+
+        //-----------------------------DRAWING------------------------------------------------
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        DrawCircle(ball.posX, ball.posY, ball._size, WHITE);
+
+
+
+
+
+        EndDrawing();
+    }
+
+
     return 0;
 }
